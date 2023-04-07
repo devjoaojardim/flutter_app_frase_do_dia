82c1f7e8385fd17125248bd844ca96fe8ea721a7
joao_ <victorsilva428@gmail.com>
2023-04-07 10:40:58 -0300

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/flutter_app_frase_do_dia/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_app_frase_do_dia
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() {
+
+  runApp( MaterialApp(
+    title: "Frases do dia",
+    home: Row(children: [
+      Text("t1"),
+      Text("t2"),
+      Text("t3"),
+    ],),
+    //home: Container(color: Colors.white, alignment: Alignment.center, ),
+  ) );
+
+}
